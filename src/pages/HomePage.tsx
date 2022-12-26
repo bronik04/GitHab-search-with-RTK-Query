473458c90c116c3d8f9267331afbde19223bed37
Nikolay Brovkin <bronik004@gmail.com>
2022-12-26 13:15:36 +0300

    feat: added dropdown

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,20 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchUsersQuery } from "../store/githab/githab.api";
-import {useDebounce} from "../hooks/debounce";
+import { useDebounce } from "../hooks/debounce";
 
 const HomePage = () => {
-    const [search, setSearch] = useState('');
-    const debounce = useDebounce(search);
+  const [search, setSearch] = useState("");
+  const [dropdown, setDropdown] = useState(false);
+  const debounce = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounce, {
+    skip: debounce.length < 3,
+  });
 
   useEffect(() => {
-      console.log(search);
-  }, [debounce]);
+    setDropdown(debounce.length > 3 && data?.length! > 0);
+  }, [debounce, data]);
 
-  const { isLoading, isError, data } = useSearchUsersQuery(debounce, {
-      skip: debounce.length < 3
-  });
-    return (
+  return (
     <div
       className={
         "flex justify-center pt-10 mx-auto h-screen w-screen bg-gray-200"
@@ -22,7 +23,6 @@ const HomePage = () => {
       {isError && (
         <p className={"text-center text-red-600"}> Somthing went wrong</p>
       )}
-      {isLoading && <p className={"text-center"}> Loading...</p>}
 
       <div className={"relative w-[560px]"}>
         <input
@@ -32,14 +32,25 @@ const HomePage = () => {
           value={search}
           onChange={(event) => setSearch(event.target.value)}
         />
-        <div
-          className={
-            "absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white"
-          }
-        >
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium,
-          asperiores.
-        </div>
+        {dropdown && (
+          <ul
+            className={
+              "absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white list-none overflow-y-scroll rounded-lg"
+            }
+          >
+            {isLoading && <p className={"text-center"}> Loading...</p>}
+            {data?.map((user) => (
+              <li
+                key={user.id}
+                className={
+                  "py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer rounded-lg"
+                }
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
